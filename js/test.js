a32bcc3122a6fb142a3f53eecf11c6a8cda0c9db
Azat <powerer000@gmail.com>
2024-03-08 22:26:40 +0500

    first commit

---
+++ js/test.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+//const result = confirm("Is Everything OK?");
+//console.log(result);
+
+
+
+//const answer = +prompt("You have 18+?", "10");
+//console.log(answer + 5);
+
+
+
+//const answers = [];
+//
+//answers[0] = prompt("Как тебя зовут?", "");
+//answers[1] = prompt("Сколько тебе лет?", "");
+//
+//document.write(answers);
+
+//---------------------------
+
+//const cat = "mouse";
+//console.log(`https://blitz.com/${cat}/5`);
+
+//console.log("https://blitz.com/" + cat);
+
+//----------------------------------
+
+//const name = [];
+//name[0] = prompt("Как тебя зовут?"); 
+//alert(`Здравствуйте ${name}`);
+
+
+//-----------------------------------
+
+let incr = 10, 
+	dincr = 10;
+
+//incr++; Если знак стоит до это префиксное значение, если после то постфиксное значение
+//dincr--; В консоль логе эти операторы работает только в префиксном значении!!!!
+
+// Знак равенства (=) это присваивание!
+// && - и, || - или, ! - отрицание 
+
+
+console.log(incr);
+console.log(dincr);
